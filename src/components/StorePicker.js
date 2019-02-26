88987c782492913411e21f8ce42d533c55b00f6f
Max <max@science.cx>
2019-02-26 19:25:26 +0100

    storePicker done

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,22 +2,28 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  //property goToStore
+  goToStore = event => {
+    event.preventDefault(); //don't submit the form!
+    this.props.history.push(`/store/${this.myInput.current.value}`); //change the page with react router. We have access to the router since StorePicker is a child
+  };
+
   render() {
     return (
-      <React.Fragment>
-        {" "}
-        {/*So that we can return sibling elements*/}
-        <form action="" className="store-selector">
-          <h2>Please enter a store</h2>
-          <input
-            type="text"
-            required
-            placeholder="Store Name"
-            defaultValue={getFunName()}
-          />
-          <button type="submit">Visit store</button>
-        </form>
-      </React.Fragment>
+      // {/* So that we can return sibling elements */}
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please enter a store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit store</button>
+      </form>
     );
   }
 }
